d091962e15ebb59964a2d1180d78e9fac10653da
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-06-04 20:28:01 +0200

    client and server refactor

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+    
+  return (
+    <div className="footer">
+      <ul>
+        <li className="footer__link">about us</li>
+        <li className="footer__link">help/faqs</li>
+      </ul>
+      <ul>
+        <li className="footer__link">comics</li>
+        <li className="footer__link">contact us</li>
+      </ul>
+    </div>
+  );
+}
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+    
+  return (
+    <div className="home">
+      <h1 className="home__title">Collectibles, Comics and More!</h1>
+
+      <div className="home__container">
+        <div className="home__container__text">
+          <p>
+            Lorem ipsum...
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
